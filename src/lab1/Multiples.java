bf2b732ca700baf927b04964a553f4a465091061
Margaret Du <46463214+margaretdu@users.noreply.github.com>
2026-09-15 14:41:36 -0400

    refactored multiples() method

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -2,10 +2,14 @@ package lab1;
 
 public class Multiples {
     public static void main() {
+        System.out.println(multiples());
+    }
+
+    private static int multiples() {
         int count3 = 1000 / 3;
         int count5 = 1000 / 5;
         int count15 = 1000 / 15;
         int count = count3 + count5 - count15;
-        System.out.println(count);
+        return count;
     }
 }
